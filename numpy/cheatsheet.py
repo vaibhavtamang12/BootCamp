2bac4ee842d5bc0c138fb30a857eae093c1d62d4
vaibhavtamang1208 <vaibhavtamang@gmail.com>
2024-06-27 12:12:02 +0530

    25/06/2024

--- numpy/cheatsheet.py
+++ numpy/cheatsheet.py
@@ -96,3 +96,72 @@ print(a)
 
 # b. Sorting along the first axis of the 2D array
 print(np.sort(a, axis = 0))
+
+
+# 8. Appending elements to array
+# 1-D Array
+# a. of a numpy array
+arr = [2, 34, 89, 90]
+arr = np.append(arr, [7])
+print(arr)
+
+# N-dimensional array
+arr = np.arange(1, 13).reshape(2, 6)
+# a. Appending column wise
+col = np.arange(5, 11).reshape(1, 6)
+arr_col = np.append(arr, col, axis=0)
+print(arr_col, "\n")
+
+# b. Appending row wise
+row = np.array([1, 2]).reshape(2, 1)
+arr_row = np.append(arr, row, axis=1)
+print(arr_row)
+
+# 9. Inserting the elements into the array
+# One-Dimensional array
+arr = np.asarray([1, 2, 3, 4])
+print("1D arr:", arr)
+print("Shape:", arr.shape)
+# Inserting value 9 at index 1
+a = np.insert(arr, 1, 9)
+print("\nArray after insertion:", a)
+print("Shape:", a.shape)
+
+# 10. Removing elements from numpy array
+# 1-D array
+print("Original arr:", arr)
+print("Shape : ", arr.shape)
+# deletion from 1D array
+object = 2
+a = np.delete(arr, object)
+print("Shape : ", a.shape)
+
+# 11. Reshaping array
+array = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+# reshaping numpy array
+# converting it to 2-D from 1-D array
+reshaped1 = array.reshape((4, array.size//4))
+# printing reshaped array
+print("First Reshaped Array:")
+print(reshaped1)
+# creating another reshaped array
+reshaped2 = np.reshape(array, (2, 8))
+# printing reshaped array
+print("\nSecond Reshaped Array:")
+print(reshaped2)
+
+# 12. Flatten a 2-D array
+list_1 = [1, 2, 3, 4]
+list_2 = [5, 6, 7, 8]
+arr = np.array([list_1, list_2])
+print(arr.flatten())
+
+# 13. Transpose a numpy array
+gfg = np.array([[1, 2], [4, 5], [7, 8]])
+print(gfg.transpose(1, 0))
+
+# 14. Combining commands
+arr1 = np.array([[1, 2], [4, 5], [7, 8], [56, 78]])
+arr2 = np.array([[8, 9], [9, 10], [10,11], [90, 100]])
+# Combining Arrays 
+np.concatenate((arr1, arr2),axis = 0)
